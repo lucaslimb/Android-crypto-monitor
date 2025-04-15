1c0681bbf4fdd8043b8fe692ab428c2d4453abcb
labsfiap <lucasdelimabzr@gmail.com>
2025-04-14 21:37:49 -0300

    add dependencia de coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,6 @@ android {
 }
 
 dependencies {
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -68,4 +66,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.activity:activity-ktx:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 }
